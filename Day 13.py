1274180b70296f41ee270b4ec963bbe26abedcfe
daveymclain <braveheart_52@hotmail.com>
2020-12-13 13:44:15 +0000

    day 13

--- Day 13.py
+++ Day 13.py
@@ -46,6 +46,20 @@ def offset_calc(bus_id_list):
     return dict_ret
 
 
+def test_ans(rev_list, bus_id_offset, test):
+    test = test - bus_id_offset[rev_list[0]]
+    for num, id in enumerate(rev_list):
+        if num == len(rev_list) - 1:
+            break
+        dif = bus_id_offset[rev_list[0]] - bus_id_offset[rev_list[num + 1]]
+        if (test - dif) % rev_list[num + 1] == 0:
+            run_2 = True
+        else:
+            run_2 = False
+            break
+    return run_2
+
+
 def main(raw_data):
     start_wait, bus_id_list = raw_data_to_list(raw_data)
     result_dict = {}
@@ -57,21 +71,11 @@ def main(raw_data):
         result_dict[bus_id] = work_out_wait_time(start_wait, bus_id)
     bus_id_lowest_wait = min(result_dict.items(), key=operator.itemgetter(1))[0]
     lowest_wait = result_dict[bus_id_lowest_wait]
-    run = True
     rev_list = bus_id_list[::-1]
     rev_list = [ x for x in rev_list if isinstance(x, int)]
     print(rev_list)
-    time = 0
-    time_increment = bus_id_list.pop(0)
     run_2 = True
     test = 0
-    # while run:
-        # while run_2:
-        #     # test = time
-        #     test += bus_id_list[-1]
-        #     if (test - bus_id_offset[bus_id_list[-1]]) % time_increment  == 0:
-        #         time = test - bus_id_offset[bus_id_list[-1]] - time_increment
-        #         break
     while run_2:
         test += rev_list[0]
         for num, id in enumerate(rev_list):
@@ -85,36 +89,8 @@ def main(raw_data):
                 break
 
     test -= bus_id_offset[rev_list[0]]
-        # while run_2:
-        #     test += rev_list[0]
-        #     for num, id in enumerate(rev_list):
-        #         if num == len(rev_list) - 2:
-        #             break
-        #         if (test - (bus_id_offset[id] - bus_id_offset[rev_list[num + 1]])) % rev_list[num + 1]:
-        #             time = test - bus_id_offset[id] - time_increment
-        #             run_2 = False
-        #         else:
-        #             run_2 = True
-        #             break
-        # run = False
-        # time += time_increment
-        # # print(time)
-        # for num, bus_ids in enumerate(bus_id_list):
-        #     previous_bus_offset = 0
-        #     if bus_ids == "x":
-        #         continue
-        #     time_dif = work_out_wait_time_offset(time + previous_bus_offset, bus_ids)
-        #
-        #     prev = bus_id_offset[bus_ids] - previous_bus_offset
-        #     if time_dif == bus_id_offset[bus_ids] - previous_bus_offset:
-        #         run = False
-        #     else:
-        #         run = True
-        #         break
-        #     previous_bus_offset = bus_id_offset[bus_ids]
+    return lowest_wait * bus_id_lowest_wait, test
 
-
-    return [lowest_wait * bus_id_lowest_wait],[test]
 
 def main_2(raw_data):
     start_wait, bus_id_list = raw_data_to_list(raw_data)
@@ -133,29 +109,7 @@ def main_2(raw_data):
     print(rev_list)
     time = 0
     time_increment = bus_id_list.pop(0)
-    run_2 = True
-    test = 0
     while run:
-        # while run_2:
-        #     # test = time
-        #     test += bus_id_list[-1]
-        #     if (test - bus_id_offset[bus_id_list[-1]]) % time_increment  == 0:
-        #         time = test - bus_id_offset[bus_id_list[-1]] - time_increment
-        #         break
-
-
-        # while run_2:
-        #     test += rev_list[0]
-        #     for num, id in enumerate(rev_list):
-        #         if num == len(rev_list) - 2:
-        #             break
-        #         if (test - (bus_id_offset[id] - bus_id_offset[rev_list[num + 1]])) % rev_list[num + 1]:
-        #             time = test - bus_id_offset[id] - time_increment
-        #             run_2 = False
-        #         else:
-        #             run_2 = True
-        #             break
-
         time += time_increment
         # print(time)
         for num, bus_ids in enumerate(bus_id_list):
@@ -171,10 +125,16 @@ def main_2(raw_data):
                 run = True
                 break
             previous_bus_offset = bus_id_offset[bus_ids]
-
-
     return [lowest_wait * bus_id_lowest_wait],[time]
+
+
 start = time.time()
-print(main(sample_2))
+ans1, ans2 = main(sample_2)
+print(ans2)
+if ans2 == 1202161486:
+    print("pass")
+else:
+    print("fail")
+
 end = time.time()
 print(end - start)
