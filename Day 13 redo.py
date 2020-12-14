a6d726fa21bfcbd673bb1587c016bdd7a25cc0ad
daveymclain <braveheart_52@hotmail.com>
2020-12-14 13:37:29 +0000

    Failed attempts.

--- Day 13 redo.py
+++ Day 13 redo.py
@@ -5,7 +5,7 @@ import DATA
 sample = """939
 7,13,x,x,59,x,31,19"""
 sample_1 = """202
-17,x,13,19"""
+1789,37,47,1889"""
 
 
 def work_out_wait_time(start_wait, bus_id):
@@ -47,16 +47,23 @@ def offset_calc(bus_id_list):
 
 
 def test_ans(rev_list, bus_id_offset, test):
-    test += bus_id_offset[rev_list[0]]
-    for num, id in enumerate(rev_list):
-        if num == len(rev_list) - 1:
-            break
-        dif = bus_id_offset[rev_list[0]] - bus_id_offset[rev_list[num + 1]]
-        if (test - dif) % rev_list[num + 1] == 0:
-            run_2 = True
-        else:
-            run_2 = False
-            break
+    offset = bus_id_offset[rev_list[0]]
+    test += offset
+    if test % rev_list[0] == 0:
+
+        for num, id in enumerate(rev_list):
+            if num == len(rev_list) - 1:
+
+                break
+            dif = bus_id_offset[rev_list[0]] - bus_id_offset[rev_list[num + 1]]
+            if (test - dif) % rev_list[num + 1] == 0:
+                run_2 = False
+                print(test)
+            else:
+                run_2 = True
+                break
+    else:
+        run_2 = True
     return run_2
 
 
@@ -71,38 +78,32 @@ def main(raw_data):
     bus_id_lowest_wait = min(result_dict.items(), key=operator.itemgetter(1))[0]
     lowest_wait = result_dict[bus_id_lowest_wait]
     rev_list = bus_id_list[::-1]
-    rev_list = [ x for x in rev_list if isinstance(x, int)]
-    print(rev_list)
-    print(bus_id_offset)
+    rev_list = [x for x in rev_list if isinstance(x, int)]
     run_2 = True
     test = 0
+    bus_id_big = sorted(rev_list)[-1]
+    # test = 100000000000 * bus_id_big
+    print("big number = {}\nrev_list = {}\noffset dict {}". format(bus_id_big, rev_list, bus_id_offset))
     while run_2:
-        test += rev_list[0]
-        for num, id in enumerate(rev_list):
-            if num == len(rev_list) - 1:
-                break
-            dif = bus_id_offset[rev_list[0]] - bus_id_offset[rev_list[num + 1]]
-            if (test - dif) % rev_list[num + 1] == 0:
-                run_2 = False
-                print(test)
-            else:
-                run_2 = True
-                break
+        test += bus_id_big
+        # print(test)
 
-    test -= bus_id_offset[rev_list[0]]
+        offset = bus_id_offset[bus_id_big]
+
+        run_2 = test_ans(rev_list, bus_id_offset, test - offset)
+
+    test -= bus_id_offset[bus_id_big]
     return lowest_wait * bus_id_lowest_wait, test
 
 
 
-if __name__ == '__main__':
+start = time.time()
+ans1, ans2 = main(sample_1)
+print(ans2)
+if ans2 == 754018:
+    print("pass")
+else:
+    print("fail")
 
-    start = time.time()
-    ans1, ans2 = main(sample_1)
-    print(ans2)
-    if ans2 == 1202161486:
-        print("pass")
-    else:
-        print("fail")
-
-    end = time.time()
-    print(end - start)
+end = time.time()
+print(end - start)
