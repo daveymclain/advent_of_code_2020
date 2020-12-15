efbe35b31a810ac047cdc4b00db6a9f20feba8c6
daveymclain <braveheart_52@hotmail.com>
2020-12-15 22:18:18 +0000

    Optimised

--- Day 15.py
+++ Day 15.py
@@ -10,33 +10,21 @@ def game(raw_data, finish_number):
     turn = 0
     turn_results_dict = {}
     last_num = 0
-    last_num_new = False
     for num in input_data:
         turn += 1
         if num not in turn_results_dict.keys():
-            turn_results_dict[num] = [turn]  # list of all the turns the number was used.
-            last_num_new = True
+            turn_results_dict[num] = turn
         last_num = num
     while turn != finish_number:
         turn += 1
-        if last_num_new:
+        if last_num not in turn_results_dict.keys():
+            turn_results_dict[last_num] = turn - 1
             ans = 0
-            last_num = ans
-            if ans not in turn_results_dict.keys():
-                turn_results_dict[ans] = [turn]
-                last_num_new = True
-            else:
-                last_num_new = False
-                turn_results_dict[ans].append(turn)
         else:
-            ans = turn_results_dict[last_num][-1] - turn_results_dict[last_num][-2]
-            last_num = ans
-            if ans not in turn_results_dict.keys():
-                turn_results_dict[ans] = [turn]
-                last_num_new = True
-            else:
-                last_num_new = False
-                turn_results_dict[ans].append(turn)
+            ans = (turn - 1) - turn_results_dict[last_num]
+            turn_results_dict[last_num] = turn - 1
+        last_num = ans
+
     return last_num
 
 
