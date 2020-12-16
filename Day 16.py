cc59597c40724003f224ab498ebd4cded0d32547
daveymclain <braveheart_52@hotmail.com>
2020-12-16 12:14:41 +0000

    part one and two complete

---
+++ Day 16.py
@@ -0,0 +1,120 @@
+import DATA
+
+sample = """class: 0-1 or 4-19
+row: 0-5 or 8-19
+seat: 0-13 or 16-19
+
+your ticket:
+11,12,13
+
+nearby tickets:
+3,9,18
+15,1,5
+5,14,9"""
+
+
+def sort_raw_data(raw_data):
+    near_tickets = []
+    ticket_fields = {}
+    data = raw_data.splitlines()
+    extract_data = data.copy()
+    for line in extract_data:
+        # sport the ranges
+        data.remove(line)
+        if line == "":
+            break
+        name, ranges = line.split(": ")
+        ranges = ranges.split(" or ")
+        ranges_list = []
+        ticket_fields[name] = ranges_list
+        for range in ranges:
+            ticket_fields[name].append(list(map(int, range.split("-"))))
+    # sort your ticket
+    del data[0]
+    my_ticket = eval(data[0])
+    del data[:3]
+    for line in data:
+        near_tickets.append(eval(line))
+    return ticket_fields, my_ticket, near_tickets
+
+
+def check_error_rate(raw_data):
+    ticket_fields, my_ticket, near_tickets = sort_raw_data(raw_data)
+    field_positions = {}
+    for field in ticket_fields:
+        field_positions[field] = []
+    print(field_positions)
+    total_errors = 0
+    temp_near_tickets = near_tickets.copy()
+    for ticket in temp_near_tickets:
+        for num in ticket:
+            valid = False
+            for ranges in ticket_fields.values():
+                if num in range(ranges[0][0], ranges[0][1] + 1):
+                    valid = True
+                    break
+                elif num in range(ranges[1][0], ranges[1][1] + 1):
+                    valid = True
+                    break
+            if not valid:
+                total_errors += num
+                near_tickets.remove(ticket)
+
+    # work out positions
+    for ticket in near_tickets:
+        for pos, num in enumerate(ticket):
+            for field in ticket_fields:
+                for ranges in ticket_fields[field]:
+                    if num in range(ranges[0], ranges[1] + 1):
+                        field_positions[field].append(pos)
+    ticket_amount = len(near_tickets)
+    field_amount = len(field_positions)
+    for pos in range(field_amount):
+        for field in field_positions.keys():
+            contents = field_positions[field]
+            count = contents.count(pos)
+            if count < ticket_amount:
+                field_positions[field] = list(filter(lambda a: a != pos, contents))
+            else:
+                for i in range(ticket_amount - 1):
+                    field_positions[field].remove(pos)
+
+    # reduce multiple entries down to one
+    # for pos in range(field_amount):
+    #     for field in field_positions:
+    #
+
+
+    # eliminate
+    print(field_positions)
+    solve_number = 0
+    while solve_number != field_amount:
+
+        print(solve_number)
+        solve_number = 0
+        for field in field_positions:
+            if len(field_positions[field]) == 1:
+                solve_number += 1
+
+                pos = field_positions[field][0]
+                # Delete position from other fields
+                skip_field = field
+                for field in field_positions:
+                    if field != skip_field:
+                        field_positions[field] = list(filter(lambda a: a != pos, field_positions[field]))
+                        # print(field_positions)
+    for field in field_positions:
+        print(field, field_positions[field])
+    departure_positions = []
+    for field in field_positions:
+        if "departure" in field:
+            departure_positions.append(field_positions[field][0])
+    part_2_ans = 1
+    for pos in departure_positions:
+        part_2_ans = part_2_ans * my_ticket[pos]
+
+    return total_errors, part_2_ans
+
+
+part_1, part_2 = check_error_rate(DATA.Day_16)
+print(part_1, part_2)
