c325ce370a7ad63a5383603d50cc4484c0f2dfac
daveymclain <braveheart_52@hotmail.com>
2020-12-18 20:54:45 +0000

    Part 2 complete

--- Day 18 redo.py
+++ Day 18 redo.py
@@ -42,12 +42,65 @@ def new_math(equation):
     return total
 
 
+def get_bracket_new(equation):
+    raw = equation
+    bracket = re.finditer(r"\([0-9+* ]+\)", equation)
+
+    for b in bracket:
+        sp = b.span()
+        contents = b.group(0)
+        first_half = raw[:sp[0]]
+        second_half = raw[sp[1]:]
+        contents = contents.replace("(", "").replace(")", "")
+        contents = new_new_math(contents)
+        raw = first_half + str(contents) + second_half
+        if ")" in raw or "(" in raw:
+            raw = get_bracket_new(raw)
+        return raw
+
+
+def new_new_math(equation):
+    equation = equation.split()
+    print(equation)
+    while len(equation) > 1:
+        for pos, value in enumerate(equation):
+            if "+" in equation:
+                if value == "+":
+                    last_num = equation.pop(pos + 1)
+                    func = equation.pop(pos)
+                    first_num = equation.pop(pos - 1)
+                    result = str(eval(first_num + func + last_num))
+                    equation.insert(pos - 1, result)
+                    break
+            else:
+                if value == "*":
+                    last_num = equation.pop(pos + 1)
+                    func = equation.pop(pos)
+                    first_num = equation.pop(pos - 1)
+                    result = str(eval(first_num + func + last_num))
+                    equation.insert(pos - 1, result)
+                    break
+
+    return int(equation[0])
+
+
+#
+# total = 0
+# for line in DATA.Day_18.splitlines():
+#     print(line)
+#     if ")" in line:
+#         total += new_math(get_bracket(line))
+#     else:
+#         total += new_math(line)
+#
+# print(total)
+
 total = 0
 for line in DATA.Day_18.splitlines():
     print(line)
     if ")" in line:
-        total += new_math(get_bracket(line))
+        total += new_new_math(get_bracket_new(line))
     else:
-        total += new_math(line)
+        total += new_new_math(line)
 
 print(total)
