ca121c067b593604fabca278e5500037ac517f24
daveymclain <braveheart_52@hotmail.com>
2020-12-18 21:12:01 +0000

    part one bug fix and tidy

--- Day 18 redo.py
+++ Day 18 redo.py
@@ -9,24 +9,6 @@ samples = {26: """2 * 3 + (4 * 5)""",
            13632: """((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"""}
 
 
-def get_bracket(equation):
-    raw = equation
-    bracket = re.finditer(r"\([0-9+* ]+\)", equation)
-
-    for b in bracket:
-        sp = b.span()
-        contents = b.group(0)
-        first_half = raw[:sp[0]]
-        second_half = raw[sp[1]:]
-        contents = contents.replace("(", "").replace(")", "")
-        contents = new_math(contents)
-        raw = first_half + str(contents) + second_half
-        if ")" in raw or "(" in raw:
-            raw = get_bracket(raw)
-
-        return raw
-
-
 def new_math(equation):
     total = 0
     equation = equation.split()
@@ -42,65 +24,62 @@ def new_math(equation):
     return total
 
 
-def get_bracket_new(equation):
+def get_bracket(equation, math):
     raw = equation
     bracket = re.finditer(r"\([0-9+* ]+\)", equation)
-
     for b in bracket:
         sp = b.span()
         contents = b.group(0)
         first_half = raw[:sp[0]]
         second_half = raw[sp[1]:]
         contents = contents.replace("(", "").replace(")", "")
-        contents = new_new_math(contents)
+        contents = math(contents)
         raw = first_half + str(contents) + second_half
         if ")" in raw or "(" in raw:
-            raw = get_bracket_new(raw)
+            raw = get_bracket(raw, math)
         return raw
+
+
+def three_num_equation(equation, pos):
+    last_num = equation.pop(pos + 1)
+    func = equation.pop(pos)
+    first_num = equation.pop(pos - 1)
+    result = str(eval(first_num + func + last_num))
+    equation.insert(pos - 1, result)
+    return equation
 
 
 def new_new_math(equation):
     equation = equation.split()
-    print(equation)
     while len(equation) > 1:
         for pos, value in enumerate(equation):
             if "+" in equation:
                 if value == "+":
-                    last_num = equation.pop(pos + 1)
-                    func = equation.pop(pos)
-                    first_num = equation.pop(pos - 1)
-                    result = str(eval(first_num + func + last_num))
-                    equation.insert(pos - 1, result)
+                    equation = three_num_equation(equation, pos)
                     break
             else:
                 if value == "*":
-                    last_num = equation.pop(pos + 1)
-                    func = equation.pop(pos)
-                    first_num = equation.pop(pos - 1)
-                    result = str(eval(first_num + func + last_num))
-                    equation.insert(pos - 1, result)
+                    equation = three_num_equation(equation, pos)
                     break
-
     return int(equation[0])
 
 
-#
-# total = 0
-# for line in DATA.Day_18.splitlines():
-#     print(line)
-#     if ")" in line:
-#         total += new_math(get_bracket(line))
-#     else:
-#         total += new_math(line)
-#
-# print(total)
+def main(math):
+    total = 0
+    for line in DATA.Day_18.splitlines():
+        if ")" in line:
+            total += math(get_bracket(line, math))
+        else:
+            total += math(line)
+    return total
 
-total = 0
-for line in DATA.Day_18.splitlines():
-    print(line)
-    if ")" in line:
-        total += new_new_math(get_bracket_new(line))
-    else:
-        total += new_new_math(line)
 
-print(total)
+start = time.time()
+print("part one ans = {}".format(main(new_math)))
+end = time.time()
+print("{} seconds".format(round(end - start, 4)))
+start_2 = time.time()
+print("part two ans = {}".format(main(new_new_math)))
+end = time.time()
+print("{} seconds".format(round(end - start_2, 4)))
+print("\ntotal time {} seconds".format(round(end - start, 4)))
