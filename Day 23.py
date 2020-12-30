a7f772c115183d87f883b85d908765c7ac7bcc26
daveymclain <braveheart_52@hotmail.com>
2020-12-30 08:57:10 +0000

    Working on part two

--- Day 23.py
+++ Day 23.py
@@ -14,55 +14,55 @@ def wrap_around_select(pos, cups):
     return pos
 
 
-def game(raw_data, amount):
-    cups = [int(i) for i in raw_data]
-    cups_len = len(cups)
-    pick_up_cups = []
-    print(cups)
+def parse_data(raw_data):
+    return [int(i) for i in raw_data]
+
+
+def game(cups, amount):
+    # print(cups)
     current_cup_pos = 0
-    current_cup = 0
-
+    sorted_cups = cups.copy()
+    sorted_cups.sort()
+    biggest_cup = sorted_cups[-1]
     for game_round in range(1, amount + 1):
-        print(f"move -{game_round}-")
-        temp_cups = cups.copy()
-        replace_cup = temp_cups.pop(current_cup_pos)
-        temp_cups.insert(current_cup_pos, "(" + str(replace_cup) + ")")
-        print(f"cups: {temp_cups}")
+        # print(f"\tGame round {game_round}")
         current_cup = cups[current_cup_pos]
-        right = 0
-        for i in range(3):
-            temp_cups = cups.copy()
-            pick_up_cups.append(temp_cups.pop(wrap_around_select(current_cup_pos + 1 + i, len(cups))))
+        pick_up_cups = []
+        # temp_cups = cups.copy()
+        # replace_cup = temp_cups.pop(current_cup_pos)
+        # temp_cups.insert(current_cup_pos, "(" + str(replace_cup) + ")")
+        # print(f"cups: {temp_cups}")
+        # start = time.time()
+        for i in range(1, 4):
+            pick_up_cups.append(cups[wrap_around_select(current_cup_pos + i, len(cups))])
+        # print("pick up cups time {}".format(time.time() - start))
+        # print(f"pick up cups {pick_up_cups}")
         for cup in pick_up_cups:
-            for pos, test_cup in enumerate(cups):
-                if cup == test_cup:
-                    cups.remove(cup)
-                    if
-
-        print(f"pick up cups {pick_up_cups}")
+            cups.remove(cup)
+        # print(time.time() - start)
         destination_cup = current_cup - 1
-        while destination_cup not in cups:
+        if destination_cup <= 0:
+            sorted_cups = cups.copy()
+            sorted_cups.sort()
+            destination_cup = sorted_cups[-1]
+        # start = time.time()
+        while destination_cup in pick_up_cups:
             destination_cup -= 1
-            if destination_cup < 0:
-                sorted_cups = cups.copy()
-                sorted_cups.sort()
-                destination_cup = sorted_cups[-1]
-        print(f"destination = {destination_cup}")
+            if destination_cup <= 0:
+                destination_cup = biggest_cup
+        destination_cup_pos = cups.index(destination_cup)
+        for cup in pick_up_cups[::-1]:
+            cups.insert(destination_cup_pos + 1, cup)
+        # print(time.time() - start)
+        # test pos
 
+        while current_cup != cups[current_cup_pos]:
+            left_cup = cups.pop(0)
+            cups.append(left_cup)
+        current_cup_pos = wrap_around_select(current_cup_pos + 1, len(cups))
+        # print(game_round)
+        # print(part_2(cups))
 
-        dif = cups_len - current_cup_pos - 1
-        less_than = destination_cup < current_cup_pos
-
-        for i in range(3):
-            for pos, cup in enumerate(cups):
-                if destination_cup == cup:
-                    destination_cup_pos = pos
-            cups.insert(wrap_around_select(destination_cup_pos + 1, cups_len), pick_up_cups.pop(-1))
-            if less_than and dif > 0:
-                cup_left = cups.pop(0)
-                cups.append(cup_left)
-                dif -= 1
-        current_cup_pos = wrap_around_select(current_cup_pos + 1, cups_len)
     return cups
 
 
@@ -70,13 +70,29 @@ def after_one(cups):
     result = ""
     for pos, cup in enumerate(cups):
         if cup == 1:
-            for i in range(len(cups) -1):
+            for i in range(len(cups) - 1):
                 pos += 1
                 result += str(cups[wrap_around_select(pos, len(cups))])
     return result
 
 
-print(after_one(game(sample, 10)))
+def generate_cups(cups, amount):
+    cups_sorted = cups.copy()
+    cups_sorted.sort()
+    highest_cup = cups_sorted[-1]
+    for i in range(amount - len(cups)):
+        highest_cup += 1
+        cups.append(highest_cup)
+    print("Length of cups {}".format(len(cups)))
+    return cups
 
 
+def part_2(cups):
+    pos = cups.index(1)
+    return cups[pos + 1] * cups[pos + 2]
 
+
+start = time.time()
+print(part_2(game(generate_cups(parse_data(data), 1000000), 100000)))
+end = time.time()
+print(end - start)
