5024ffaf23c5f4b07fbf1dda3b70305ff233d710
daveymclain <braveheart_52@hotmail.com>
2021-01-01 15:54:48 +0000

    Working on part two

--- Day 23 redo.py
+++ Day 23 redo.py
@@ -25,40 +25,62 @@ def game(cups, amount):
     current_cup_pos = 0
     biggest_cup = len(cups)
     for game_round in range(1, amount + 1):
-        print(f"game round {game_round}")
-        # start = time.time()
+        start = time.time()
         current_cup = cups[current_cup_pos]
         pick_up_cups = []
+        # removing pick up cups
         for i in range(1, 4):
             pick_up_cups.append(cups[wrap_around_select(current_cup_pos + i, biggest_cup)])
         offset = 0
+        time_making_pickup_list = time.time()
+
         for cup in pick_up_cups:
-            # cups.remove(cup)
             cups_length = len(cups)
             if current_cup_pos + 1 < cups_length:
                 del cups[current_cup_pos + 1]
             else:
                 del cups[wrap_around_select(current_cup_pos + 1 - offset, cups_length)]
                 offset += 1
-        destination_cup = current_cup - 1
+        time_removing_cups = time.time()
 
+        destination_cup = current_cup - 1
         if destination_cup <= 0:
             destination_cup = biggest_cup
-
         while destination_cup in pick_up_cups:
             destination_cup -= 1
             if destination_cup <= 0:
                 destination_cup = biggest_cup
-        # print(time.time() - start)
-        # start =time.time()
+        time_destinations = time.time()
+
         destination_cup_pos = cups.index(destination_cup)
-        # print(time.time() - start)
+        time_destinations_index = time.time()
+
         for cup in pick_up_cups[::-1]:
             cups.insert(destination_cup_pos + 1, cup)
+        time_insert_cups = time.time()
+
         while current_cup != cups[current_cup_pos]:
             left_cup = cups.popleft()
             cups.append(left_cup)
+        time_adjust = time.time()
+
         current_cup_pos = wrap_around_select(current_cup_pos + 1, biggest_cup)
+        round_time = time.time() - start
+        if round_time > .01:
+            print(f"round {game_round}")
+            print(f"total time {round_time}")
+            pick_up = time_making_pickup_list - start
+            print(f"making pick up list {pick_up}")
+            removing_cups = time_removing_cups - time_making_pickup_list
+            print(f"removing cups = {removing_cups}")
+            workout_destination_cup = time_destinations - time_removing_cups
+            print(f"time to work out destination {workout_destination_cup}")
+            find_index = time_destinations_index - time_destinations
+            print(f"time to find index {find_index}")
+            insert_cups = time_insert_cups - time_destinations_index
+            print(f"time to insert {insert_cups}")
+            adjust = time_adjust - time_insert_cups
+            print(f"time to adjust {adjust}")
 
     return cups
 
@@ -86,6 +108,6 @@ def part_2(cups):
     pos = cups.index(1)
     return cups[pos + 1] * cups[pos + 2]
 start = time.time()
-print(part_2(game(generate_cups(parse_data(data), 1000000), 100000)))
+print(after_one(game(generate_cups(parse_data(data), 0), 100)))
 end = time.time()
 print(end - start)
