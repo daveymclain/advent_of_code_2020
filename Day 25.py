ae2cd58484fd7311f856fdd2e192e3e53e6ff93d
daveymclain <braveheart_52@hotmail.com>
2021-01-04 12:30:45 +0000

    Tidy

--- Day 25.py
+++ Day 25.py
@@ -1,4 +1,4 @@
-sample = 5764801
+import time
 
 door = 16616892
 card = 14505727
@@ -11,16 +11,16 @@ def find_loop_size(to_find):
         value = value % 20201227
         if value == to_find:
             return loop_size
-        if loop_size % 1000 == 0:
-            print("loop number = {}\n".format(loop_size))
+
 
 def find_key(loops, subect_number):
     value = 1
     for loop_size in range(loops):
         value = value * subect_number
         value = value % 20201227
-        if loop_size % 1000 == 0:
-            print("loop number = {}\n".format(loop_size))
     return value
 
-print(find_key(find_loop_size(door), card))
+
+start = time.time()
+print("part one {}".format(find_key(find_loop_size(door), card)))
+print("time taken {}".format(time.time() - start))
