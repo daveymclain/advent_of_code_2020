0965e94e7f25a3d9f01f1b5e9617195d39fd1972
daveymclain <braveheart_52@hotmail.com>
2021-01-04 12:32:48 +0000

    typo and tidy

--- Day 25.py
+++ Day 25.py
@@ -6,17 +6,19 @@ card = 14505727
 
 def find_loop_size(to_find):
     value = 1
-    for loop_size in range(1, 1000000000):
+    loop_size = 0
+    while 1:
+        loop_size += 1
         value = value * 7
         value = value % 20201227
         if value == to_find:
             return loop_size
 
 
-def find_key(loops, subect_number):
+def find_key(loops, subject_number):
     value = 1
     for loop_size in range(loops):
-        value = value * subect_number
+        value = value * subject_number
         value = value % 20201227
     return value
 
